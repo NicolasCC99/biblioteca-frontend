b6f0d9b1543bbc6b69090f2cee732414a153a0c8
Nicolas Cabrera <nacho09_99hotmail.com>
2025-11-06 17:40:48 -0300

    cambios en app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,26 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Rutas que deben renderizarse en el cliente
+  {
+    path: 'admin',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'student',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'books/add',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'books/edit/:id',
+    renderMode: RenderMode.Client
+  },
+  // El resto de las rutas pueden ser prerenderizadas
   {
     path: '**',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender
   }
 ];
